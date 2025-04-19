5fcce44b0cc41060737886403ff8ae09a4295bac
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-19 20:00:17 +0100

    minOperations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This function calculates the fewest number of operations
+(only Copy All and Paste) to get exactly n 'H' characters.
+"""
+
+def minOperations(n):
+    steps = 0
+    i = 2
+    while n > 1:
+        if n % i == 0:
+            steps += i
+            n = n // i
+        else:
+            i += 1
+    return steps
